5faa05c348c54f0b4b5a66851a6782c6eb4dd609
Alfredo P G Maranca <Apmaranca@gmail.com>
2024-03-08 13:55:25 -0300

    Update main_script.py

--- CLI/main_script.py
+++ CLI/main_script.py
@@ -1,3 +1,14 @@
 import helper_module
 
+// IPFS initialization
 helper_module.helper_function()
+ipfs_client = IPFSClient()
+ipfs_client.start_ipfs_daemon()
+
+
+# Your program logic here
+
+
+// Do at program exit
+ipfs_client = IPFSClient()
+ipfs_client.stop_ipfs_daemon()
